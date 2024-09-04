1bf3a827ef3c784a886c1e65e9b180951c966e6a
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 17:39:13 -0500

    .\6-print_string\: Fixed output formatting

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine("{0}{1}{2}\n{3:}\n", str, str, str, str.Substring(0, 9));
+		Console.WriteLine("{0}{1}{2}\n{3:}", str, str, str, str.Substring(0, 9));
         }
 }
