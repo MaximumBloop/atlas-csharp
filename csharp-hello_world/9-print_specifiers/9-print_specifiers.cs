a6c542cee8e04e029972d030b45ff44806cb50c7
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 17:24:00 -0500

    .\9-print_specifiers\: Removed unnecessary files

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -3,10 +3,10 @@ using System.Globalization;
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		double percent = .7553;
+    static void Main(string[] args)
+    {
+        double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:P}\nCurrency: {1:C}", percent, currency);
-	}
+    }
 }
