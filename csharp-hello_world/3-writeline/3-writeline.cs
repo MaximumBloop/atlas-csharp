09fe6e5b480a04950fdea2c59db813779e05a99c
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 14:15:06 -0500

    .\3-writeline\: Demonstrates basic 'Hello World' script

---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
