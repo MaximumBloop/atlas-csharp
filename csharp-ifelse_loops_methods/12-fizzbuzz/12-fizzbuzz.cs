a0f1bc88acbd85ded86e0f9f76442c85674b1e67
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 23:38:53 -0500

    .\12-fizzbuzz\: Fizzbuzz!

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1";
+            for (int i = 2; i <= 100; i++)
+            {
+                str += ' ';
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    if (i % 3 == 0)
+                    {
+                        str += "Fizz";
+                    }
+                    if (i % 5 == 0)
+                    {
+                        str += "Buzz";
+                    }
+                } else
+                {
+                    str += i.ToString();
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
