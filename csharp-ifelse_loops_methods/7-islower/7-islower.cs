cb7a3f2ed22e18af194cc4b104b60b10c17d6c07
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 22:25:35 -0500

    .\7-islower\: Recreated 'Char.IsLower()' method

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c >= 97 && c <= 122)
+        {
+            return true;
+        }
+        return false;
+    }
+}
