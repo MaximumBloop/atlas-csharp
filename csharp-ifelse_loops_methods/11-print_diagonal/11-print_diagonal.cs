64f29775fb53e5dcf3383e109d0fbee347e8d9f6
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 23:28:37 -0500

    .\11-print_diagonal\: Prints a diagonal line in the terminal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            string str = "";
+            for (int j = 0; j < i; j++)
+            {
+                str += ' ';
+            }
+            Console.WriteLine("{0}\\$", str);
+        }
+        Console.Write("\n");
+    }
+}
