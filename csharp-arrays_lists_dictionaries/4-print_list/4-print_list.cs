4744dbd070b45fae96a84ed160c42d5d6adbe50e
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-05 11:28:52 -0500

    .\4-print_list\: Basic usage of Lists

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> list = new List<int>();
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            if (list.Count > 0)
+            {
+                Console.Write(' ');
+            }
+            list.Add(i);
+            Console.Write(i);
+        }
+        Console.Write('\n');
+        return list;
+    }
+}
