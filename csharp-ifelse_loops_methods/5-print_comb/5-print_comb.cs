08867b6eaf83cf83bfb1b5ffdb9ff58de489fa91
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 21:18:56 -0500

    .\5-print_comb\: Fixed output issue

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,7 +7,7 @@ namespace _5_print_comb
         static void Main(string[] args)
         {
             string str = "00";
-            for (int i = 0; i <= 99; i++)
+            for (int i = 1; i <= 99; i++)
             {
                 str += String.Format(", {0:00}", i);
             }
