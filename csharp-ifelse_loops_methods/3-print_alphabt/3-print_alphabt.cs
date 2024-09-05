a191ae0a336e11d433d37f0478e7f1d6fd9dd567
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 20:53:57 -0500

    .\3-print_alphabt\: Oooo loops AND if statements?

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (char character in "abcdefghijklmnopqrstuvwxyz")
+            {
+                if (character != 'q' && character != 'e')
+                {
+                    Console.Write(character);
+                }
+            }
+        }
+    }
+}
