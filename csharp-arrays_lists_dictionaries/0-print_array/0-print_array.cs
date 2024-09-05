9038a5c472502ba5af21abf8707749428fe7e771
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-05 09:00:42 -0500

    .\0-print_array\: Creates populated array and prints it

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+        }
+
+        Console.Write("0");
+        for (int i = 1; i < size; i++)
+        {
+            Console.Write(" {0}", array[i]);
+        }
+        Console.Write('\n');
+        return array;
+    }
+}
