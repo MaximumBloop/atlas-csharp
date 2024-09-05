be7aa1a3cdafe31408adbdcbe0b1b57598bc85a7
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 22:13:34 -0500

    .\6-print_comb2\: Finer comb that produces every possible combination of 2 digits (no repeat digits)

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = i + 1; j <= 9; j++)
+                {
+                    if (str.Length > 0)
+                    {
+                        str += ", ";
+                    }
+                    str += String.Format("{0}{1}", i, j);
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
