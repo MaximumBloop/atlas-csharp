f545e9a48a89c461a416f365b954b4fd09e39b63
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-05 15:13:00 -0500

    .\6-divisible_by_2\: Lists divisibility of a list

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> boolList = new List<bool>();
+        if (myList.Count == 0)
+        {
+            return boolList;
+        }
+
+        foreach(int item in myList)
+        {
+            if (item % 2 == 0)
+            {
+                boolList.Add(true);
+            } else
+            {
+                boolList.Add(false);
+            }
+        }
+        return boolList;
+    }
+}
