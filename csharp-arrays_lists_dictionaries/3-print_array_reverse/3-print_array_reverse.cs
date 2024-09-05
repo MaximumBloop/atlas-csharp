05915d41681b5eaec0476f8e81aca2a889c66486
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-05 11:07:34 -0500

    .\3-print_array_reverse\: Prints an array in reverse

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.Write('\n');
+            return;
+        } else
+        {
+            Console.Write("{0}", array[array.Length - 1]);
+        }
+        for (int i = array.Length - 2; i >= 0; i--)
+        {
+            Console.Write(" {0}", array[i]);
+        }
+        Console.Write('\n');
+    }
+}
