c816a09a3cd476b2089d308b59b40982505012b2
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-04 21:17:34 -0500

    .\5-print_comb\

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "00";
+            for (int i = 0; i <= 99; i++)
+            {
+                str += String.Format(", {0:00}", i);
+            }
+            str += '\n';
+            Console.Write(str);
+        }
+    }
+}
