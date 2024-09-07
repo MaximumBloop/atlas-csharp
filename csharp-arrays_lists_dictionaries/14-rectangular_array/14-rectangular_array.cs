8397dfa82ce80f996d23223c03faf1e4a93828d7
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-07 14:38:39 -0500

    .\14-rectangular_array\: Prints a 5x5 array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int[,] array = new int[5,5];
+        array[2,2] = 1;
+        int i = 0;
+        foreach (int value in array)
+        {
+            Console.Write(value);
+
+            i++;
+
+            if (i % 5 == 0)
+            {
+                Console.Write("\n");
+            } else
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
