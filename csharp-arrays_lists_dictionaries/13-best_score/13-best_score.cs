82a31cbe8fb7cdf90293d2bdf1c9bedb7cc0d4bf
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-07 12:20:15 -0500

    .\13-best_score\: Gets the max value of a dictionary

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+        string maxkey = null;
+        foreach (string key in myList.Keys)
+        {
+            maxkey = key;
+            break;
+        }
+        foreach(string key in myList.Keys)
+        {
+            if (myList[key] > myList[maxkey])
+            {
+                maxkey = key;
+            }
+        }
+        return maxkey;
+    }
+}
