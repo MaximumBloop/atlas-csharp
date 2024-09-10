6b5d991f7f4d31a3db26b7be6f6d0d2d629ea38b
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-10 00:09:26 -0500

    .\0-unique_add\: Adds unique integers of a list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> uniq = new List<int>();
+
+        foreach (var i in myList)
+        {
+            bool isUnique = true;
+            foreach (var j in uniq)
+            {
+                if (i == j)
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique)
+            {
+            uniq.Add(i);
+            }
+        }
+        int sum = 0;
+        foreach (var i in uniq)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
