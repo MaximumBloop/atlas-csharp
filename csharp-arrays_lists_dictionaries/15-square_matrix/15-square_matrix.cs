12a087652cfb9fe8333d669e7cd00d58ecfa51f1
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-09 23:38:43 -0500

    .\15-square_matrix\: Squares all indices of a matrix

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] copy = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+
+        for (int i = 0; i < copy.GetLength(0); i++)
+        {
+            for (int j = 0; j < copy.GetLength(1); j++)
+            {
+                copy[i,j] = myMatrix[i, j] * myMatrix[i, j];
+            }
+        }
+        return copy;
+    }
+}
