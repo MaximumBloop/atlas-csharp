1c31ff7453dbba20343bb2bd64b94e549b345871
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 16:45:16 -0500

    .\10-linkedlist_get_node\: Gets value of node at specified index

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < myLList.Count && i < n; i++)
+        {
+            if (current == null)
+            {
+                return 0;
+            }
+            current = current.Next;
+        }
+        return current.Value;
+    }
+}
