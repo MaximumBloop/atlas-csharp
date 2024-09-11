3133708d2720a7dc19d28a42acb458db0fad58af
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 17:32:25 -0500

    .\12-linkedlist_insert\: Inserts new nodes into llist while retaining sort

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        LinkedListNode<int> current = myLList.First;
+
+        if (newNode.Value <= current.Value)
+        {
+            myLList.AddFirst(newNode);
+            return newNode;
+        }
+
+        while (current.Next != null && current.Value < newNode.Value)
+        {
+            current = current.Next;
+        }
+
+        if (current.Next == null && current.Value < newNode.Value)
+        {
+            myLList.AddAfter(current, newNode);
+        } else
+        {
+            myLList.AddBefore(current, newNode);
+        }
+        return (newNode);
+    }
+}
