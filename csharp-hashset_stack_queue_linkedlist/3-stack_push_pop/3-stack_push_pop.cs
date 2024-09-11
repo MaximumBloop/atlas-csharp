6911e2a8f9fef0494ce2564a759212d156194d1d
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 15:13:21 -0500

    .\3-stack_push_pop\: Shows count and top object of stack, checks for a search variable in stack, and pops items up to and including that search variable

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        } else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+        if (aStack.Contains(search))
+        {
+            // this is literally impossible to do without multiple .Pop() calls or some stupid workaround. So, here's my stupid workaround!
+            Action popAction = () => aStack.Pop();
+            while (aStack.Peek() != search)
+            {
+                popAction();
+            }
+            popAction();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
