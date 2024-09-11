9a994a371b240f9e9a83d1a88ed554ef16ede888
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 12:36:28 -0500

    .\1-common_elements\: Performs a union on two lists

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> unionList = new List<int>();
+        
+        int maxCount = list1.Count > list2.Count ? list1.Count : list2.Count;
+        List<int> bigList = list1.Count > list2.Count ? list1 : list2;
+        List<int> smallList = list1.Count > list2.Count ? list2 : list1;
+        for (int i = 0; i < maxCount; i++)
+        {
+            if (smallList.Contains(bigList[i]))
+            {
+                unionList.Add(bigList[i]);
+            }
+        }
+        unionList.Sort();
+        return unionList;
+    }
+}
