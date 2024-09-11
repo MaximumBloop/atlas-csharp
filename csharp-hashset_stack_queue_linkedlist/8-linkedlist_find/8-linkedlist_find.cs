4fe0a75bbe47668185d7620ee5818601fa37421b
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 16:33:17 -0500

    .\8-linkedlist_find\: Gets the index of a value in a llist

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = -1;
+        if (!(myLList.Contains(value)))
+        {
+            return -1;
+        }
+        for (LinkedListNode<int> node = myLList.First; node != null; node = node.Next)
+        {
+            i++;
+            if (node.Value == value)
+            {
+                break;
+            }
+        }
+        return i;
+    }
+}
