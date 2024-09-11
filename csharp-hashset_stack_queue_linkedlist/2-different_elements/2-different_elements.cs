fa0b0b814678a571a4f8c8ce0f8f652f0dd088d1
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 14:51:15 -0500

    .\2-different_elements\: Gets the difference between two lists

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> unionList = new List<int>();
+        List<int> differenceList = new List<int>();
+        
+        int maxCount = list1.Count > list2.Count ? list1.Count : list2.Count;
+        List<int> bigList = list1.Count > list2.Count ? list1 : list2;
+        List<int> smallList = list1.Count > list2.Count ? list2 : list1;
+        for (int i = 0; i < maxCount; i++)
+        {
+            if (smallList.Contains(bigList[i]))
+            {
+                unionList.Add(bigList[i]);
+            }
+        }
+        unionList.Sort();
+
+        foreach (int item in unionList)
+        {
+            if (smallList.Contains(item))
+            {
+                smallList.Remove(item);
+            }
+            if (bigList.Contains(item))
+            {
+                bigList.Remove(item);
+            }
+        }
+        differenceList.AddRange(bigList);
+        differenceList.AddRange(smallList);
+        differenceList.Sort();
+        return differenceList;
+    }
+}
