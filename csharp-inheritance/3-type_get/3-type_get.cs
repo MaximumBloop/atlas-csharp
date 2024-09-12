a852cc248974d7bcddd16b97dc98d0c384466025
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-11 23:25:02 -0500

    .\3-type_get\

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+
+        foreach (PropertyInfo p in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(p.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+
+        foreach (MethodInfo m in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
