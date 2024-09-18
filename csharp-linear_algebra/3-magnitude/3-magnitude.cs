e7aa0fe0012e1bd6fb6d088075428e3928778051
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-18 13:22:14 -0500

    .\3-magnitude\

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return -1;
+        }
+
+        double result = 0;
+
+        foreach (double i in vector)
+        {
+            result += i * i;
+        }
+
+        return Math.Round(Math.Sqrt(result), 2);
+    }
+}
