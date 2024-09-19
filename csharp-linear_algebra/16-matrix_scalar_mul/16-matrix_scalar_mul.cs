8f000ff4dc4d89a7c5fd89a42b1cf118596e9fc4
MaximumBloop <keepforeverstrong@gmail.com>
2024-09-18 21:04:38 -0500

    .\16-matrix_scalar_mul\

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.GetLength(0) < 2 ||
+            matrix.GetLength(1) < 2)
+        {
+            return new double[,] { { -1} };
+        }
+        
+        double [,] result = new double[matrix.GetLength(0),matrix.GetLength(1)];
+        Array.Copy(matrix, result, matrix.Length);
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i,j] *= scalar;
+            }
+        }
+
+        return result;
+    }
+}
